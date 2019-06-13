aa5c67b50df8659b04219a1f0e0eb8bca01f0494
k-westbrook <kcheriewestbrook@gmail.com>
2019-06-12 22:40:06 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { render } from 'react-dom';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//const API= api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&APPID={APIKEY}
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      longitude: null,
+      latitude: null,
+      weather: null
+    }
+    this.getLocation = this.getLocation.bind(this);
+    this.getWeatherInfo = this.getWeatherInfo.bind(this);
+  }
+
+  async getWeatherInfo() {
+    console.log(this.state)
+    let response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${this.state.latitude}&lon=${this.state.longitude}&APPID=48f52744023d2822c0dc7e4697b0de4f`);
+
+    let weatherInfo = await response.data;
+    console.log(weatherInfo)
+  }
+
+  getLocation() {
+    let locationObject = this;
+
+
+    function displayLocationInfo(position) {
+      const lng = position.coords.longitude;
+      const lat = position.coords.latitude;
+      console.log(lng, lat)
+
+      locationObject.setState({
+        latitude: lat,
+        longitude: lng
+      });
+
+    }
+
+    if (navigator.geolocation) {
+
+      navigator.geolocation.getCurrentPosition(displayLocationInfo, (err) => {
+        console.log(err);
+      });
+    } else {
+      console.log("No geolocation");
+    }
+  }
+
+  componentDidMount() {
+    this.getLocation();
+    this.getWeatherInfo();
+  }
+  render() {
+
+
+    return (
+      <div className="App">
+        <header className="App-header">
+
+          <h3>Weather</h3>
+          {(this.state.weather) ?
+            <p>{this.state.weather}</p>
+            :
+            <p>Loading Weather..</p>
+          }
+        </header>
+      </div >
+    );
+  }
 }
 
 export default App;
+
+
+
+
+
+
+
+
